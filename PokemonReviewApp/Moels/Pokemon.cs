4fad56b3a80b1b8f330cacad5d614c2c734c567d
AliMahammadov <e.mehemmedov99@gmail.com>
2024-10-23 14:49:44 +0400

    Add project files.

---
+++ PokemonReviewApp/Moels/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PokemonReviewApp.Moels
+{
+    public class Pokemon
+    {
+        public int Id{ get; set; }
+
+        public string Name{ get; set; }
+
+        public DateTime BirtyDate { get; set; }
+    }
+}
